c5e341ac26435775a2541e92f76eafde4595476f
Mrinmoy_Nanda <mrinmoy.nanda2002@gmail.com>
2026-06-01 22:54:25 +0530

    Updated app and added Docker deployment

--- app.py
+++ app.py
@@ -78,4 +78,6 @@ def process_voice():
 
 # Run App
 if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 7860))
+        app.run(host="0.0.0.0", port=port)
